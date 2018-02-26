a60eca70455f418690b9210232194d010531468b
Uy Nguyen Long <nluy@FOSSIL.COM>
2018-02-26 09:24:02 +0700

    Backup.

--- WeMusic/ViewController.swift
+++ WeMusic/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let url = URL.init(string: "http://api.soundcloud.com/tracks/293/stream")
+//        let playerItem = AVPlayerItem.init(url: url!)
+//        
+//            player = AVPlayer.init(playerItem: playerItem)
+//            guard let player = player else { return }
+//            
+//        
+//            player.play()
+//            print("Playing music")
+        
+        
+        //let url = Bundle.main.url(forResource: "I2Y", withExtension: "mp3")!
+        let data = NSData.init(contentsOf: URL.init(string: "http://api.soundcloud.com/tracks/293/stream")!)
+        do {
+            player = try AVAudioPlayer.init(data: data as! Data)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+            print("Playing music")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
